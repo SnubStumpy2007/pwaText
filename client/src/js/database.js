78e0d83fe81d9b672d41b99905d18b7d621cfb73
SnubStumpy2007 <joshua192007@gmail.com>
2023-10-14 21:06:41 -0400

    update

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  
+  try {
+    const newItem = { content, timestamp: Date.now() };
+    const key = await store.add(newItem);
+    console.log(`Added item with key: ${key}`);
+  } catch (error) {
+    console.error('Error adding item:', error);
+  } finally {
+    await tx.done;
+  }
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  
+  try {
+    const contentList = await store.getAll();
+    return contentList;
+  } catch (error) {
+    console.error('Error getting content:', error);
+    return [];
+  } finally {
+    await tx.done;
+  }
+};
+
 
 initdb();
